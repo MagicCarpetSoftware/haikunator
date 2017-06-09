3f0d47ff8aa7831d6ea9f5b7aaeca66665746460
Paul Bernays <paul@bernays.org>
2017-06-09 16:47:05 +1000

    Made it pseudo-random, added more words

--- lib/haikunator.rb
+++ lib/haikunator.rb
@@ -1,56 +1,64 @@
 require "haikunator/version"
-require "securerandom"
 
 module Haikunator
   class << self
-    def haikunate(token_range = 9999, delimiter = "-")
+    def haikunate(str, token_range = 9999, delimiter = "-")
+      @prng = Random.new(str.to_s.gsub(/\H/, "").to_i(16))
       build(token_range, delimiter)
     end
 
     private
 
     def build(token_range, delimiter)
-      sections = [
-        adjectives[random_seed % adjectives.length],
-        nouns[random_seed % nouns.length],
-        token(token_range)
-      ]
-
-      sections.compact.join(delimiter)
+      [
+        adjectives[random(adjectives.length)],
+        nouns[random(nouns.length)],
+        random(token_range)
+      ].compact.join(delimiter)
     end
 
-    def random_seed
-      SecureRandom.random_number(4096)
-    end
-
-    def token(range)
-      SecureRandom.random_number(range) if range > 0
+    def random(range)
+      @prng.rand(range) unless range < 1
     end
 
     def adjectives
-      %w(
-        autumn hidden bitter misty silent empty dry dark summer
-        icy delicate quiet white cool spring winter patient
-        twilight dawn crimson wispy weathered blue billowing
-        broken cold damp falling frosty green long late lingering
-        bold little morning muddy old red rough still small
-        sparkling throbbing shy wandering withered wild black
-        young holy solitary fragrant aged snowy proud floral
-        restless divine polished ancient purple lively nameless
-      )
+      %w[
+        aged amazing ancient autumn awesome bare beautiful big
+        billowing bitter black blue bold bright broken clear
+        cold cool crimson crisp crystal damp dark dawn delicate
+        delightful divine dry elegant empty enormous fading
+        fallen falling fantastic fine floral fragrant fresh
+        frosty gentle golden gracious green handsome hidden holy
+        hot icy joyful late lingering little lively lonely long
+        lovely marvelous merciful misty morning muddy nameless
+        old painful patient peaceful perfect polished pretty
+        proud pure purple quiet raw red refreshing restful
+        restless rough sacred secluded sharp shy silent simple
+        sleepy small smooth snowy soaring soft solitary sparkling
+        spring startling still stunning summer surprising sweet
+        tender throbbing tiny tranquil twilight unexpected unusual
+        wandering warm weathered weightless wet white wild winter
+        wispy withered wonderful young
+      ]
     end
 
     def nouns
-      %w(
-        waterfall river breeze moon rain wind sea morning
-        snow lake sunset pine shadow leaf dawn glitter forest
-        hill cloud meadow sun glade bird brook butterfly
-        bush dew dust field fire flower firefly feather grass
-        haze mountain night pond darkness snowflake silence
-        sound sky shape surf thunder violet water wildflower
-        wave water resonance sun wood dream cherry tree fog
-        frost voice paper frog smoke star
-      )
+      %w[
+        air amber apple bamboo bell bird blood breadcrumb breeze
+        brook bush butterfly candle canyon cave cherry cloud color
+        creek crescent darkness dawn dew diamond dove dream dust
+        evening face feather field fire firefly flower fog forest
+        frog frost glade glitter grass hand haze heart hill ice
+        journey kitten lake leaf leave life light marionette meadow
+        mirror monk moon moonlight morning mountain nighfall night
+        orb origami paper pebble petal pine pond puppy rain rainbow
+        resonance retreat river rock sand scroll sea seed shadow
+        shape silence skin sky smile smoke snow snowflake someone
+        sound spring star stone summer sun sunset sunshine surf
+        sword tea temple thunder tree violet voice water waterfall
+        wave wheelhouse whisper wildflower wildness wind wood
+        world zen
+      ]
     end
   end
 end
--- spec/lib/haikunator_spec.rb
+++ spec/lib/haikunator_spec.rb
@@ -2,38 +2,38 @@ require_relative "../spec_helper"
 
 describe Haikunator do
   it "generates a name like still-silence-5012" do
-    name = Haikunator.haikunate
+    name = Haikunator.haikunate("a")
 
     expect(name).to match(/\A\w+-\w+-\d{1,4}\z/)
   end
 
   it "won't return the same name for subsequent calls" do
-    name1 = Haikunator.haikunate
-    name2 = Haikunator.haikunate
+    name1 = Haikunator.haikunate("a")
+    name2 = Haikunator.haikunate("b")
 
     expect(name1).not_to eql(name2)
   end
 
   it "permits optional configuration of the token range" do
-    name = Haikunator.haikunate(9)
+    name = Haikunator.haikunate("a", 9)
 
     expect(name).to match(/-\d{1}\z/)
   end
 
   it "drops the token if token range is 0" do
-    name = Haikunator.haikunate(0)
+    name = Haikunator.haikunate("a", 0)
 
     expect(name).to match(/\A\w+-\w+\z/)
   end
 
   it "permits optional configuration of the delimiter" do
-    name = Haikunator.haikunate(9999, ".")
+    name = Haikunator.haikunate("a", 9999, ".")
 
     expect(name).to match(/\A\w+\.\w+\.\d{1,4}\z/)
   end
 
   it "drops the token and delimiter if token range is 0 and delimiter empty space" do
-    name = Haikunator.haikunate(0, " ")
+    name = Haikunator.haikunate("a", 0, " ")
 
     expect(name).to match(/\A\w+ \w+\z/)
   end
